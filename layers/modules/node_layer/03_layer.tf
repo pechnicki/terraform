27f8670e2ef34b2962ad3cfb3134bb180a333ae6
Yuri Pechnicki <yuri.pechnicki@gmail.com>
2023-02-01 14:55:13 -0300

    atualiando para object_version

--- layers/modules/node_layer/03_layer.tf
+++ layers/modules/node_layer/03_layer.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_layer_version" "node_layer" {
-  layer_name = var.layer_name
-  s3_bucket = data.aws_s3_object.artifact.bucket
-  s3_key = data.aws_s3_object.artifact.key
-  compatible_runtimes = ["nodejs16.x","nodejs14.x","nodejs12.x"]
-  source_code_hash = data.aws_s3_object.artifact.etag
+  layer_name          = var.layer_name
+  s3_bucket           = data.aws_s3_object.artifact.bucket
+  s3_key              = data.aws_s3_object.artifact.key
+  s3_object_version   = data.aws_s3_object.artifact.version_id
+  compatible_runtimes = ["nodejs16.x", "nodejs14.x", "nodejs12.x"]
 }
