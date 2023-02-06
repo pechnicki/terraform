9fa3691ae59d332a4824ffcb3d4dc7ab788975ea
Yuri Pechnicki <yuri.pechnicki@gmail.com>
2023-02-06 12:53:10 -0300

    adicionando code_hash

--- layers/modules/node_layer/03_layer.tf
+++ layers/modules/node_layer/03_layer.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_layer_version" "node_layer" {
   layer_name          = var.layer_name
   s3_bucket           = data.aws_s3_object.artifact.bucket
   s3_key              = data.aws_s3_object.artifact.key
-  s3_object_version   = data.aws_s3_object.artifact.version_id
+  source_code_hash    = data.aws_s3_object.artifact.body
   compatible_runtimes = ["nodejs18.x", "nodejs16.x", "nodejs14.x", "nodejs12.x"]
 }
