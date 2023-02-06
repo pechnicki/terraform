7e44dab14b517a5fa6b7a8a8b62e9f66a8204298
Yuri Pechnicki <yuri.pechnicki@gmail.com>
2023-02-06 17:07:43 -0300

    removendo acesso publico dos buckets

--- infraestrutura/09_s3_buckets.tf
+++ infraestrutura/09_s3_buckets.tf
@@ -10,6 +10,15 @@ resource "aws_s3_bucket_acl" "acl" {
   acl      = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  for_each                = toset(var.s3_buckets)
+  bucket                  = aws_s3_bucket.bucket[each.value].id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_versioning" "versioning" {
   for_each = toset(var.s3_buckets)
   bucket   = aws_s3_bucket.bucket[each.value].id
