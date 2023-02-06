91d0cfd493f51635bd79ec3a0f30cab6b2fe18bc
Yuri Pechnicki <yuri.pechnicki@gmail.com>
2023-02-06 17:20:12 -0300

    modificando para obter hash da tag

--- layers/modules/node_layer/03_layer.tf
+++ layers/modules/node_layer/03_layer.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_layer_version" "node_layer" {
   layer_name          = var.layer_name
   s3_bucket           = data.aws_s3_object.artifact.bucket
   s3_key              = data.aws_s3_object.artifact.key
-  source_code_hash    = data.aws_s3_object.artifact.body
+  source_code_hash    = data.aws_s3_object.artifact.tags["source_code_hash"]
   compatible_runtimes = ["nodejs18.x", "nodejs16.x", "nodejs14.x", "nodejs12.x"]
 }
